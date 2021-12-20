c710d608bdb122a75fbdb7cc043c5aa4459f6102
curtkim <curtkim@users.noreply.github.com>
2021-12-20 04:24:53 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="curtkim"
+original_name="fastapi_awesome"
+original_urlname="fastapi-awesome"
+original_description="Awesome fastapi_awesome created by curtkim"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastapi_awesome $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
